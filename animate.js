29ebc3a2e482db471007162a6163bfb3e61236c0
Nicholas Yang <nick@nicholasyang.com>
2016-03-14 10:23:22 -0400

    added better animate function

--- animate.js
+++ animate.js
@@ -1,12 +1,13 @@
 
-
+var j, k
 /*
 Up = 0
 Right = 1
 Down = 2
 Left = 3
 
- */
+*/
+
 var slide = function slide(direction, tileX, tileY)
 {
     var posX = tileX;
@@ -36,6 +37,41 @@ var slide = function slide(direction, tileX, tileY)
 
             }
             drawTile(tileX, tileY, posX, posY);
+            slideCode = window.requestAnimationFrame(slideHelper);
+        }
+    }
+
+};
+
+var slide2 = function slide2(direction, x0, y0, x1, y1)
+{
+    var posX = x0;
+    var posY = y0;
+    var slideHelper = function slideHelper(){
+        if (posX != x1 && posY != y1)
+        {
+            ctx.clearRect(100 * posX + 8, 100 * posY + 8, 100, 100);
+            switch (direction){
+            case 0:
+                // up
+                ctx.translate(0, -1);
+                break;
+            case 1:
+                // right
+                ctx.translate(1, 0)
+                break;
+            case 2:
+                // down
+                ctx.translate(0, 1)
+                break;
+            case 3:
+                // left
+                ctx.translate(-1, 0)
+                break;nn
+
+
+            }
+            drawTile(x0, y0, posX, posY);
             slideCode = window.requestAnimationFrame(slideHelper);
         }
     }
