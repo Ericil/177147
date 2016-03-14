2d7579d117550fd34077c511c1877cedb3786088
Nicholas Yang <nick@nicholasyang.com>
2016-03-14 10:24:21 -0400

    minor fix

--- animate.js
+++ animate.js
@@ -8,42 +8,9 @@ Left = 3
 
 */
 
-var slide = function slide(direction, tileX, tileY)
-{
-    var posX = tileX;
-    var posY = tileY;
-    var slideHelper = function slideHelper(){
-        if (x < tileX + 1 &&  y < tileY + 1 && x > tileX - 1 && y > tileY - 1)
-        {
-            ctx.clearRect(100 * posX + 8, 100 * posY + 8, 100, 100);
-            switch (direction){
-            case 0:
-                // up
-                posY = posY - 1;
-                break;
-            case 1:
-                // right
-                posX = posX + 1;
-                break;
-            case 2:
-                // down
-                posY = posY + 1;
-                break;
-            case 3:
-                // left
-                posX = posX - 1;
-                break;
 
 
-            }
-            drawTile(tileX, tileY, posX, posY);
-            slideCode = window.requestAnimationFrame(slideHelper);
-        }
-    }
-
-};
-
-var slide2 = function slide2(direction, x0, y0, x1, y1)
+var slide = function slide2(direction, x0, y0, x1, y1)
 {
     var posX = x0;
     var posY = y0;
